2022c4e1766d94b052e08f063608e22d148f9df0
Arynthon <arin.toaca@gmail.com>
2025-01-10 17:20:48 +0200

    Initial hacker-themed portfolio

---
+++ script.js
@@ -0,0 +1,4 @@
+// Display the current year in the footer
+const currentYearElement = document.getElementById("current-year");
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = currentYear;
